365b14e6ebe4e7ba07d3dc5c938f49d3c105439a
imparasparabiya <137284876+imparasparabiya@users.noreply.github.com>
2023-06-25 18:58:28 +0530

    Add files via upload

---
+++ CH3_ARIT.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+  clrscr();
+  int a,b;
+  a=25;
+  b=19;
+
+  printf("Value of A=25 && B=19\n");
+
+  printf("Sum of A+B + = %d\n",a+b);
+
+  printf("Subtraction of A-B = %d\n",a-b);
+
+  printf("Asterisk of A*B = %d\n",a*b);
+
+  printf("Dicided by A/B = %d\n",a/b);
+
+  printf("Modular of A%%B = %d\n",a%b);
+
+  getch();
+
+}
